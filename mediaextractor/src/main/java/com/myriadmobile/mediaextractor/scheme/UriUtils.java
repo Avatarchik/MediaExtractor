7495a7a55c4c66d04eff32879b0c76fcf8eba2da
Justin Tuchek <jtuchek@outlook.com>
2015-06-07 13:28:44 -0500

    resolve type of uri to a potential of declared, supported scheme types.

--- mediaextractor/src/main/java/com/myriadmobile/mediaextractor/scheme/UriUtils.java
+++ mediaextractor/src/main/java/com/myriadmobile/mediaextractor/scheme/UriUtils.java
@@ -1,10 +1,12 @@
 package com.myriadmobile.mediaextractor.scheme;
 
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class UriUtils {
 
+    @NonNull
     public static Type getUriType(@Nullable Uri uri) throws UnsupportedSchemeException {
         Type mediaType = Type.valueFromScheme(uri != null ? uri.getScheme() : null);
         if(mediaType != null) {
