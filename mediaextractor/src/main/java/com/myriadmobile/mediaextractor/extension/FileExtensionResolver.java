3f5a1be4715da4b83c988630f43fcb87616caf7a
Justin Tuchek <jtuchek@outlook.com>
2015-06-07 15:29:16 -0500

    implementation for resolving the extension of a file:// uri scheme.

--- mediaextractor/src/main/java/com/myriadmobile/mediaextractor/extension/FileExtensionResolver.java
+++ mediaextractor/src/main/java/com/myriadmobile/mediaextractor/extension/FileExtensionResolver.java
@@ -1,17 +1,24 @@
 package com.myriadmobile.mediaextractor.extension;
 
 import android.net.Uri;
+import android.webkit.MimeTypeMap;
 
+import com.myriadmobile.mediaextractor.scheme.Type;
 import com.myriadmobile.mediaextractor.scheme.UnsupportedSchemeException;
+import com.myriadmobile.mediaextractor.scheme.UriUtils;
 
 public class FileExtensionResolver implements ExtensionTypeResolver {
+
     @Override
     public void checkUri(Uri uri) throws UnsupportedSchemeException {
-
+        if(UriUtils.getUriType(uri) != Type.FILE) {
+            throw new UnsupportedSchemeException(Type.FILE);
+        }
     }
 
     @Override
     public String resolveExtensionType(Uri uri) throws UnsupportedSchemeException {
-        return null;
+        checkUri(uri);
+        return MimeTypeMap.getFileExtensionFromUrl(uri.toString());
     }
 }
