ade37b582248feead5fbe8128ed295d7b2909629
Justin Tuchek <jtuchek@outlook.com>
2015-06-07 15:28:45 -0500

    now able to resolve the extension of a content:// uri scheme.

--- mediaextractor/src/main/java/com/myriadmobile/mediaextractor/extension/ContentExtensionResolver.java
+++ mediaextractor/src/main/java/com/myriadmobile/mediaextractor/extension/ContentExtensionResolver.java
@@ -1,17 +1,33 @@
 package com.myriadmobile.mediaextractor.extension;
 
+import android.content.Context;
 import android.net.Uri;
+import android.webkit.MimeTypeMap;
 
+import com.myriadmobile.mediaextractor.mime.ContentMimeTypeResolver;
+import com.myriadmobile.mediaextractor.scheme.Type;
 import com.myriadmobile.mediaextractor.scheme.UnsupportedSchemeException;
+import com.myriadmobile.mediaextractor.scheme.UriUtils;
 
 public class ContentExtensionResolver implements ExtensionTypeResolver {
+
+    private final Context context;
+    private final ContentMimeTypeResolver mimeTypeResolver;
+
+    public ContentExtensionResolver(Context context) {
+        this.context = context;
+        this.mimeTypeResolver = new ContentMimeTypeResolver(context);
+    }
+
     @Override
     public void checkUri(Uri uri) throws UnsupportedSchemeException {
-
+        if(UriUtils.getUriType(uri) != Type.CONTENT) {
+            throw new UnsupportedSchemeException(Type.CONTENT);
+        }
     }
 
     @Override
     public String resolveExtensionType(Uri uri) throws UnsupportedSchemeException {
-        return null;
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeTypeResolver.resolveMimeType(uri));
     }
 }
